67f2cffe7675f6182e786dac22c4a1fd9f828620
Constantine Seleznyoff <const.seoff@gmail.com>
2017-11-30 04:08:46 +0300

    Промежуточный коммит

--- src/AppBundle/Resources/public/js/common.js
+++ src/AppBundle/Resources/public/js/common.js
@@ -1,5 +1,17 @@
-var santa = angular.module('santa', []);
-santa.config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-});
+var santa = angular
+    .module('santa', [])
+    .config(function ($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+    }).directive('a', function () {
+        return {
+            restrict: 'E',
+            link: function ($scope, element, attrs) {
+                if (attrs.ngClick || '#' === attrs.href) {
+                    element.click(function (e) {
+                        e.preventDefault();
+                    });
+                }
+            }
+        };
+    });
